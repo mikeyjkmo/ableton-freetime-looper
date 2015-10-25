4bd537c907429dc8fc2a51faab40d355dc1bb938
Mikey Mo <fadeout7@gmail.com>
2015-10-25 21:49:32 +0000

    RunningState now passes messages to the loopTracker when _dequeuAndSendAll() is called.
    Also, RunningState now sends of "restart" messages at every interval.

--- States/RunningState.cpp
+++ States/RunningState.cpp
@@ -11,13 +11,28 @@ namespace AbletonProject
         _asyncTimer.start();
     }
 
+    /*
+     * _dequeueAndSendAll() is called at every interval as set on the AsyncTimer
+     * (which will be the length of the initial loop) until the state
+     * changes.
+     */
     void RunningState::_dequeueAndSendAll()
     {
         std::unique_ptr<Message> message;
         while (_queue.tryDequeue(message))
         {
             _resources.messageDispatcher.sendMidiMessage(message.get());
+            // inform the loopTracker that a command has been received
+            _resources.loopTracker.commandReceived(message);
         }
+
+        // Get vector of restart messages to send
+        for (auto* restartMessage : _resources.loopTracker.getNextRestartMessages())
+        {
+            _resources.messageDispatcher.sendMidiMessage(restartMessage);
+        }
+
+        _resources.loopTracker.incrementInterval();
     }
 
     void RunningState::handle(std::unique_ptr<StateBase>& state, std::unique_ptr<Message> message)
