3d1385f88ea7a91fc954d0264ade283a66588082
NathanC <nathan.lb.cooper@outlook.com>
2016-02-14 19:09:36 +0000

    Add CommandMappings Stub.

---
+++ LiveFreetimeLooper.FreetimeLooper/Messaging/CommandMappings.cpp
@@ -0,0 +1,6 @@
+#include "CommandMappings.hpp"
+
+namespace LiveFreetimeLooper
+{
+
+}
---
+++ LiveFreetimeLooper.FreetimeLooper/Messaging/CommandMappings.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace LiveFreetimeLooper
+{
+    class CommandMappings final
+    {
+        // todo collection of all the loop messages we know and all their respective stop message
+
+        // somecollection< startmessage, stoppMessage >
+
+        // todo some kind of isMapped() function
+    };
+}
