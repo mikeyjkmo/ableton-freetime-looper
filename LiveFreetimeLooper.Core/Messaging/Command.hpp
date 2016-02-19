3fb0b4514b1cfe591193478e659163655a265f53
NathanLBCooper <nathan.lb.cooper@outlook.com>
2016-02-19 11:59:47 +0000

    COMMAND_ID_SIZE belongs to Command
    I dislike global constants

--- LiveFreetimeLooper.Core/Messaging/Command.hpp
+++ LiveFreetimeLooper.Core/Messaging/Command.hpp
@@ -5,12 +5,12 @@
 
 namespace LiveFreetimeLooper
 {
-    // The first n items in Command::content that identify the Command
-    const int COMMAND_ID_SIZE = 2;
-
     class Command final
     {
     public:
+        // The first n items in Command::content that identify the Command
+        const static int COMMAND_ID_SIZE = 2;
+
         Command(const std::vector<unsigned char>& content) : content(content)
         {
             if (content.size() < 2)
@@ -48,7 +48,7 @@ namespace std
         {
             // Hash just based on command content
             std::size_t seed = 0;
-            for (int i = 0; i < LiveFreetimeLooper::COMMAND_ID_SIZE; ++i)
+            for (int i = 0; i < LiveFreetimeLooper::Command::COMMAND_ID_SIZE; ++i)
             {
                 seed ^= k.content[i] + 0x9e3779b9 + (seed << 6) + (seed >> 2);
             }
