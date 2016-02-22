4fa2eed14a58a0ade08868f1856d9d29c1301698
NathanLBCooper <nathan.lb.cooper@outlook.com>
2016-02-22 15:37:11 +0000

    Refactor smoke test

--- LiveFreetimeLooper.Tests/StateTests/StateTests.cpp
+++ LiveFreetimeLooper.Tests/StateTests/StateTests.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <chrono>
 #include <thread>
+#include <stdexcept>
 
 #include "../Mocks/MockEventLogger.hpp"
 #include "../Mocks/MockMessageDispatcher.hpp"
@@ -21,128 +22,199 @@
 
 using namespace LiveFreetimeLooper;
 
+class SmokeTest
+{
+private:
+    MockEventLogger _loggerMock;
+    MockMessageDispatcher _dispatcherMock;
+    LoopTracker _loopTracker;
+    MockAsyncTimerFactory _asyncTimerFactory;
+
+    StateResources _resources;
+    std::unique_ptr<StateBase> _state;
+
+    MockAsyncTimer* _timer;
+    bool _timerHasBeenInitialised = false;
+
+    MockAsyncTimer* getTimer()
+    {
+        if (!_timerHasBeenInitialised)
+        {
+            if (_asyncTimerFactory.getCreatedTimersWeakRefs().size() < 1)
+                throw std::runtime_error("No timer has been created");
+
+            _timerHasBeenInitialised = true;
+            _timer = _asyncTimerFactory.getCreatedTimersWeakRefs().back();
+        }
+        return _timer;
+    }
+public:
+    SmokeTest() :
+        _loggerMock(),
+        _dispatcherMock(),
+        _loopTracker(),
+        _asyncTimerFactory(),
+        _resources(_dispatcherMock, _loopTracker, _loggerMock, _asyncTimerFactory),
+        _state(std::make_unique<CreatedState>(_resources)),
+        _timer(nullptr)
+    {
+    }
+
+    void sendStartMessage(Command command)
+    {
+        _state->handle(_state, std::make_unique<StartMessage>(command));
+    }
+
+    void sendStopMessage(Command command, const Command& startCommand)
+    {
+        _state->handle(_state, std::make_unique<StopMessage>(command, startCommand));
+    }
+
+    void sendStdIn()
+    {
+       _state->handleStdin(_state, std::string("this is a string"));
+    }
+
+    size_t getNumberOfDispatchedCommands()
+    {
+        return _dispatcherMock.getDispatchedCommands().size();
+    }
+
+    Command getLastCommandDispatched()
+    {
+        return _dispatcherMock.getDispatchedCommands().back();
+    }
+
+    void stepTimer()
+    {
+        getTimer()->step();
+    }
+
+    template<class T>
+    bool stateIsOfType()
+    {
+        return dynamic_cast<T*>(_state.get()) != nullptr;
+    }
+
+};
+
+Command createCommand(unsigned char data)
+{
+    std::vector<unsigned char> content = { data, 1 };
+    return Command(content);
+}
+
 TEST_CASE("Smoke test")
 {
-    MockEventLogger loggerMock;
-    MockMessageDispatcher dispatcherMock;
-    LoopTracker loopTracker;
-    MockAsyncTimerFactory asyncTimerFactory;
-
-    StateResources resources(dispatcherMock, loopTracker, loggerMock, asyncTimerFactory);
-    std::unique_ptr<StateBase> state = std::make_unique<CreatedState>(resources);
-
-    auto sendStart = [&state](Command command)
+    SmokeTest smokeTest;
+    auto REQUIRE_CorrectNumberOfMessagesDispatched = [&smokeTest](size_t number)
     {
-        state->handle(state, std::make_unique<StartMessage>(command));
+        REQUIRE(smokeTest.getNumberOfDispatchedCommands() == number);
+    };
+    auto REQUIRE_LastCommand = [&smokeTest] (const Command& command)
+    {
+        REQUIRE(smokeTest.getLastCommandDispatched().content == command.content);
     };
 
-    auto sendStop = [&state](Command command, const Command& startCommand)
-    {
-        state->handle(state, std::make_unique<StopMessage>(command, startCommand));
-    };
-
-    auto numberOfDispatchedCommands = [&dispatcherMock]
-    {
-        return dispatcherMock.getDispatchedCommands().size();
-    };
-
-    std::vector<unsigned char> quantisableCommandContent = { 0, 1 };
-    std::vector<unsigned char> unrelatedCommandContent = { 12, 3 };
-    Command quantisableCommand(quantisableCommandContent);
-    Command unrelatedCommand(unrelatedCommandContent);
+    REQUIRE(smokeTest.stateIsOfType<CreatedState>());
 
     // Send StdIn to move to InitialLoopWaitingState
-    state->handleStdin(state, std::string("this is a string"));
-    REQUIRE(dynamic_cast<InitialLoopWaitingState*>(state.get()));
+    smokeTest.sendStdIn();
+    REQUIRE(smokeTest.stateIsOfType<InitialLoopWaitingState>());
+
+    Command quantisableCommand(createCommand(1));
+    Command unrelatedCommand(createCommand(2));
 
     // Send in the first message, to move to InitialLoopState
-    sendStart(quantisableCommand);
+    smokeTest.sendStartMessage(quantisableCommand);
+    REQUIRE(smokeTest.stateIsOfType<InitialLoopState>());
+    REQUIRE_CorrectNumberOfMessagesDispatched(1);
+    REQUIRE(smokeTest.getLastCommandDispatched().content == quantisableCommand.content);
 
-    REQUIRE(dynamic_cast<InitialLoopState*>(state.get()));
-    REQUIRE(numberOfDispatchedCommands() == 1);
-    REQUIRE(dispatcherMock.getDispatchedCommands().back().content == quantisableCommand.content);
-
-    sendStart(unrelatedCommand);
-
-    REQUIRE(dynamic_cast<InitialLoopState*>(state.get()));
-    REQUIRE(numberOfDispatchedCommands() == 1);
+    // Send in an unrelated message, to no effect
+    smokeTest.sendStartMessage(unrelatedCommand);
+    REQUIRE(smokeTest.stateIsOfType<InitialLoopState>());
+    REQUIRE_CorrectNumberOfMessagesDispatched(1 + 0);
 
     // Send in the second message, to move to RunningState
-    sendStart(quantisableCommand);
-
-    REQUIRE(dynamic_cast<RunningState*>(state.get()));
-    REQUIRE(numberOfDispatchedCommands() == 2);
-    REQUIRE(dispatcherMock.getDispatchedCommands().back().content == quantisableCommand.content);
-
-    auto timer = asyncTimerFactory.getCreatedTimersWeakRefs().back();
+    smokeTest.sendStartMessage(quantisableCommand);
+    REQUIRE(smokeTest.stateIsOfType<RunningState>());
+    REQUIRE_CorrectNumberOfMessagesDispatched(1 + 1);
+    REQUIRE_LastCommand(quantisableCommand);
 
     // The quantised loop restart message is being dispached once per step 
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 3);
-    REQUIRE(dispatcherMock.getDispatchedCommands().back().content == quantisableCommand.content);
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 4);
-    REQUIRE(dispatcherMock.getDispatchedCommands().back().content == quantisableCommand.content);
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 5);
-    REQUIRE(dispatcherMock.getDispatchedCommands().back().content == quantisableCommand.content);
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(2 + 1);
+    REQUIRE_LastCommand(quantisableCommand);
+
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(3 + 1);
+    REQUIRE_LastCommand(quantisableCommand);
+
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(4 + 1);
+    REQUIRE_LastCommand(quantisableCommand);
 
     // Introduce more loops
     // => A) Loop of length 1 (original quantised) + B) Loop of length 1 + C) Loop of length 2
-    std::vector<unsigned char> loopOfLengthOneCommandContent = { 2, 54 };
-    std::vector<unsigned char> loopOfLengthTwoCommandContent = { 24, 4 };
-    Command loopOfLengthOneCommand(loopOfLengthOneCommandContent);
-    Command loopOfLengthTwoCommand(loopOfLengthTwoCommandContent);
+    Command loopBCommand(createCommand(3));
+    Command loopCCommand(createCommand(4));
 
-    sendStart(loopOfLengthOneCommand);
-    REQUIRE(numberOfDispatchedCommands() == 5);
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 7);
-    sendStart(loopOfLengthTwoCommand);
-    sendStart(loopOfLengthOneCommand);
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 11);
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 13);
-    sendStart(loopOfLengthTwoCommand);
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 17);
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 19); // +2: A and B restart 
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 22); // +3: A, B and C restart
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 24); // +2
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 27); // +3
+    smokeTest.sendStartMessage(loopBCommand); // Start recording B
+    REQUIRE_CorrectNumberOfMessagesDispatched(5 + 0); // Nothing sent until next timer step
 
-    // Stop loopOfLengthOne introduced loop
-    std::vector<unsigned char> stopCommandContent = { 31, 9 };
-    Command stopCommand(stopCommandContent);
-    sendStop(stopCommand, loopOfLengthOneCommand);
-    REQUIRE(numberOfDispatchedCommands() == 28); // +1 stop message
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 29); // +1 A restarts
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 31); // +2 A and C restarts
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 32); // +1
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 34); // +2
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(5 + 2); // Message just sent + A restart
 
-    // Re-introduce loopOfLengthOne
-    sendStart(loopOfLengthOneCommand);
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 36); // +1 and B command 
-    sendStart(loopOfLengthOneCommand);
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 40); // +4 A, B, C and B command
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 42); // +2 A, B
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 45); // +3 A, B, C
-    timer->step();
-    REQUIRE(numberOfDispatchedCommands() == 47); // +2 A, B
+    smokeTest.sendStartMessage(loopCCommand); // Start recording C
+    smokeTest.sendStartMessage(loopBCommand); // Stop recording B
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(7 + 4); // Messages just sent + A and B restart
+
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(11 + 2); // A and B restart
+
+    smokeTest.sendStartMessage(loopCCommand);
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(13 + 4);  // Message just sent + A, B and C restart
+
+    // Loops A, B and C are working correctly
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(17 + 2); // A and B restart
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(19 + 3); // A, B and C restart
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(22 + 2); // A and B restart
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(24 + 3); // A, B and C restart
+
+    // Stop loop B
+    Command stopCommand(createCommand(5));
+    smokeTest.sendStopMessage(stopCommand, loopBCommand);
+    REQUIRE_CorrectNumberOfMessagesDispatched(27 + 1); // Message just sent
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(28 + 1); // A restart
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(29 + 2); // A and C restart
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(31 + 1); // A restart
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(32 + 2); // A and C restart
+
+    // Re-introduce loop B
+    smokeTest.sendStartMessage(loopBCommand);
+    REQUIRE_CorrectNumberOfMessagesDispatched(34 + 0);
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(34 + 2); // Message just sent and A restart
+    smokeTest.sendStartMessage(loopBCommand);
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(36 + 4);  // Message just sent, A, B and C restart
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(40 + 2);  // A and B restart
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(42 + 3);  // A, B and C restart
+    smokeTest.stepTimer();
+    REQUIRE_CorrectNumberOfMessagesDispatched(45 + 2);  // A and B restart
 }
 
 // //Uses std::this_thread::sleep_for and measures outputs based on actual time.
